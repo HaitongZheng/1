91666dd0c96b0c89a802fd74699927909a680edb
Haitong32 <haitongzheng32@gmail.com>
2019-04-26 03:10:48 -0400

    update-thesis2

--- script-home.js
+++ script-home.js
@@ -1,81 +1,16 @@
 $(document).ready(function(){
-  $("#construction").hide();
-  $( "#cross" ).click(function() {
-     $("#tags").fadeToggle();
-     $(".information").fadeToggle("slow", "linear");
-  });
-
-
-  var value = 0
-  $("#cross").rotate({
-    bind:
-    {
-      click: function(){
-      value +=45;
-      $(this).rotate({ animateTo:value})}
-    }
-  });
-
-
-  $("#cross").on('click', function() {
-    var click = $(this).data('clicks');
-
-    if (click) {
-        $('.tags').animate({left: "+=8vw"});
-        $('.content').animate({left: "+=8vw"});
-    }else{
-        $('.tags').animate({left: "-=8vw"});
-        $('.content').animate({left: "-=8vw"});
-    };
-
-    $(this).data('clicks', !click);
-
-  });
-
-  $("#ABOUT").mouseover(function(){
-    $("#ellipse1").css("fill" , "yellow")
-  });
-  $("#ABOUT").mouseout(function(){
-    $("#ellipse1").css("fill" , "none")
-  });
-  $("#CONTACT").mouseover(function(){
-    $("#ellipse2").css("fill" , "yellow")
-  });
-  $("#CONTACT").mouseout(function(){
-    $("#ellipse2").css("fill" , "none")
-  });
-
-$("#bookdesign").click(function(){
-    $("#work2, #work3, #work4, #work5, #work6, #work7, #work12").fadeToggle();
-    $(this).toggleClass("tagshover");
-    $(".tag").not("#t1").toggleClass("opacity");
-    $('html,body').animate({ scrollTop: $(".header").offset().top + 200});
+$("#work00").mouseover(function(){
+  $(this).css("opacity" , ".80")
 });
-$("#webdesign").click(function(){
-    $("#work1, #work3, #work4, #work5, #work6, #work7, #work8, #work9, #work10, #work11, #work12").fadeToggle();
-    $(this).toggleClass("tagshover");
-    $(".tag").not("#t2").toggleClass("opacity");
-    $('html,body').animate({ scrollTop: $(".header").offset().top + 200});
+$("#work00").mouseout(function(){
+  $(this).css("opacity" , "1")
 });
-$("#animation").click(function(){
-    $("#work1, #work2, #work5, #work6, #work7, #work8, #work9, #work10, #work11, #work12").fadeToggle();
-    $(this).toggleClass("tagshover");
-    $(".tag").not("#t3").toggleClass("opacity");
-    $('html,body').animate({ scrollTop: $(".header").offset().top + 200});
+$("#work0").mouseover(function(){
+  $(this).css("opacity" , ".80")
 });
-$("#campaign").click(function(){
-    $("#work1, #work2, #work3, #work4, #work8, #work9, #work10, #work11, #work12").fadeToggle();
-    $(this).toggleClass("tagshover");
-    $(".tag").not("#t4").toggleClass("opacity");
-    $('html,body').animate({ scrollTop: $(".header").offset().top + 200});
+$("#work0").mouseout(function(){
+  $(this).css("opacity" , "1")
 });
-$("#photography").click(function(){
-    $("#work1, #work2, #work3, #work4, #work5, #work6, #work7, #work8, #work9, #work10, #work11").fadeToggle();
-    $(this).toggleClass("tagshover");
-    $(".tag").not("#t5").toggleClass("opacity");
-    $('html,body').animate({ scrollTop: $(".header").offset().top + 200});
-});
-
 $("#work1").mouseover(function(){
   $(this).css("opacity" , ".80")
 });
